64094ebfb75ebd377c2c036874c0be1dfdb3720b
netaasree <bnssrs05@gmail.com>
2026-01-29 22:40:05 +0530

    Deployment changes

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,5 @@
-// src/socket.js
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 
-const ENDPOINT = "http://localhost:5000";
-
-export const socket = io(ENDPOINT, {
+export const socket = io(import.meta.env.VITE_API_URL, {
   autoConnect: false,
 });
